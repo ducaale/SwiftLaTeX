f5fd715283f5683798a2f1fe0965e2c7f0b27d88
Mohamed Daahir <sharaf.13@hotmail.com>
2023-12-13 00:55:41 +0200

    move engine classes to one file

--- index.mjs
+++ index.mjs
@@ -7,25 +7,35 @@ const EngineStatus = {
   Error: 4
 }
 
-const ENGINE_PATH = 'swiftlatexxetex.js';
-
-export default class XeTeXEngine {
+class LaTeXEngine {
   latexWorker = undefined;
   latexWorkerProxy = undefined;
   latexWorkerStatus = EngineStatus.Init;
+
+  constructor({extension, endpoint, workerPath}) {
+    self.extension = extension
+    self.endpoint = endpoint
+    self.workerPath = workerPath
+  }
 
   async loadEngine() {
     if (this.latexWorker !== undefined) {
       throw new Error('Other instance is running, abort()');
     }
+
     this.latexWorkerStatus = EngineStatus.Init;
-    this.latexWorker = new Worker(ENGINE_PATH);
+
+    this.latexWorker = new Worker(self.workerPath);
     await new Promise((resolve) => {
       this.latexWorker.onmessage = (event) => {
         if (event.data === 'wasm_initialized') { resolve(); }
       }
     });
+
     this.latexWorkerProxy = Comlink.wrap(this.latexWorker);
+    await this.latexWorkerProxy.setExtension(self.extension);
+    await this.latexWorkerProxy.setTexliveEndpoint(self.endpoint);
+
     this.latexWorkerStatus = EngineStatus.Ready;
   }
 
@@ -46,6 +56,19 @@ export default class XeTeXEngine {
     console.log('Engine compilation start');
     const startTime = performance.now();
     const {pdf, log, status} = await this.latexWorkerProxy.compileLaTeXRoutine();
+    this.latexWorkerStatus = EngineStatus.Ready;
+    console.log('Engine compilation finish ' + (performance.now() - startTime));
+
+    return { status, log, pdf: new Uint8Array(pdf) };
+  }
+
+  async compilePDF() {
+    this.checkEngineStatus();
+    this.latexWorkerStatus = EngineStatus.Busy;
+
+    console.log('Engine compilation start');
+    const startTime = performance.now();
+    const {pdf, log, status} = await this.latexWorkerProxy.compilePDFRoutine();
     this.latexWorkerStatus = EngineStatus.Ready;
     console.log('Engine compilation finish ' + (performance.now() - startTime));
 
@@ -98,5 +121,35 @@ export default class XeTeXEngine {
       this.latexWorker = undefined;
       this.latexWorkerProxy = undefined;
     }
+  }
+}
+
+export class XeTeXEngine extends LaTeXEngine {
+  constructor() {
+    super({
+      extension: '.xdv',
+      endpoint: 'https://texlive2.swiftlatex.com/xetex/',
+      workerPath: 'xetex.wasm/swiftlatexxetex.js'
+    })
+  }
+}
+
+export class DvipdfmxEngine extends LaTeXEngine {
+  constructor() {
+    super({
+      extension: '.pdf',
+      endpoint: 'https://texlive2.swiftlatex.com/xetex/',
+      workerPath: 'dvipdfm.wasm/swiftlatexdvipdfm.js'
+    })
+  }
+}
+
+export class PdfTeXEngine extends LaTeXEngine {
+  constructor() {
+    super({
+      extension: '.pdf',
+      endpoint: 'https://texlive2.swiftlatex.com/pdftex/',
+      workerPath: 'pdftex.wasm/swiftlatexpdftex.js'
+    })
   }
 }
